c9033527f699069c8d1d219db7329f884ae16693
marinakreplak <marina.kreplak@polymtl.ca>
2017-10-26 19:34:22 +0200

    Add file reading

---
+++ importFile.py
@@ -0,0 +1,36 @@
+from scipy.io import arff
+from cStringIO import StringIO
+import numpy as np
+
+
+
+
+def readFile(file):
+    data, meta = arff.loadarff(file)
+    return data, meta
+
+
+file = "datasets/adult.arff"
+data, meta = readFile(file)
+print(meta)
+meta.types()
+print(meta.names())
+
+data2 = np.zeros(shape = (len(data),len(data[0])))
+
+for i in range(0, len(meta.names())):
+    if meta.types()[i] == 'numeric':
+        for j in range(0, len(data)):
+            data2[j][i] = data[j][i]
+    else:
+        values = meta[meta.names()[i]][1]
+        for j in range(0, len(data)):
+            if data[j][i] == '?':
+                data2[j][i] = -1
+            else:
+                 data2[j][i] = values.index(data[j][i])
+
+print data2
+
+
+
