6da2e0231ccb97c2056b0b5ff953553292a0cbd8
santi <santi_bt@hotmail.com>
2017-11-03 19:54:54 +0100

    MODIFY utils, bisection_kmeans

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import cmath
 # This function returns the euclidean distance between the two given points
 def distance(p1, p2):
-    sum = 0
+    sumatory = 0
     for i in range(len(p1)):
-        sum += (p1[i] - p2[i])^2
-    return math.sqrt(sum)
+        sumatory += (p1[i] - p2[i])^2
+    return cmath.sqrt(sumatory)
